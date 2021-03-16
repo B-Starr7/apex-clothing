e33c9fd961a668d28202a8db5c685057a7da6798
Ben Starr <bcstarr47@gmail.com>
2021-03-15 20:42:06 -0700

    added stripe payment integration, updated favicon and title

---
+++ src/components/stripe-button/stripe-button.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IVOF5KFtPgL6Etox3NVYAC2OID9joxls7uvDgEG7Jq9g1ER3sRgivh3t7LyAEiHwarLwjZolWc5573Z6eqohM6U001dshznKm";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Apex Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
